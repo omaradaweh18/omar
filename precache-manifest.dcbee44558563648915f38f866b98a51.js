a46b0fc30b5500967878b4a6a1cc08c9985cd4c7
omaradaweh18 <48047144+omaradaweh18@users.noreply.github.com>
2020-01-08 09:17:48 -0800

    Add files via upload

---
+++ precache-manifest.dcbee44558563648915f38f866b98a51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "512dc5ca81c7fee2ea830a75d0cb7e88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09212daf97752ff3840b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "757c9ed8a44573f34f97",
+    "url": "/static/js/2.cae258c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.cae258c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09212daf97752ff3840b",
+    "url": "/static/js/main.c595a5d7.chunk.js"
+  },
+  {
+    "revision": "26df6be03cb194bf33e2",
+    "url": "/static/js/runtime-main.177e397a.js"
+  }
+]);
